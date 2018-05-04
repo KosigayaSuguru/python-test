8bd39cb5d3c567c367d7c3c6293bb3dfeb386ce5
self2 <self2>
2018-05-05 00:05:01 +0900

    flask-testのコントローラの分割

--- flask-test/__init__.py
+++ flask-test/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from . import route1
 
 app = Flask(__name__)
+
 
 @app.route('/hello')
 def hello_world():
     return "Hello, world"
+
+
+app.register_blueprint(route1.app_route1)
