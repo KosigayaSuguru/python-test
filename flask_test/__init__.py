795ce767bc1bd517a513b2401d506bf758278b3f
self2 <self2>
2018-05-05 16:28:09 +0900

    flask-testに例外ハンドリング処理を追加

--- flask_test/__init__.py
+++ flask_test/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_test.controllers import route1
+from flask_test.controllers import error_handler
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
@@ -9,4 +11,32 @@ def hello_world():
     return "Hello, world"
 
 
+@app.route('/error_handle_test_badrequest')
+def error_handle_test_badrequest():
+
+    """エラーハンドリング確認用\n
+    ※register_error_handler(BadRequest)でBadRequest例外を処理する確認\n
+    """
+    raise BadRequest("my BadRequest message")
+
+
+@app.route('/error_handle_test')
+def error_handle_test():
+
+    """エラーハンドリング確認用\n
+    ※register_error_handler(Exception)で例外を全部処理する確認\n
+    ※BadRequest例外は、専用の例外ハンドラに行くのも確認\n
+    """
+
+    raise Exception("error_handle_test")
+
+
+# コントローラをルーティングに追加
 app.register_blueprint(route1.app_route1)
+
+# エラーハンドラ追加（とりこぼした例外を全て処理する）
+app.register_error_handler(Exception, error_handler.error)
+
+# エラーハンドラ追加（BadRequest例外を処理する）
+app.register_error_handler(
+    BadRequest, error_handler.error_badrequest)
---
+++ flask_test/controllers/error_handler.py
@@ -0,0 +1,14 @@
+def error(ex):
+
+    """例外に対して、固有のメッセージ（error_handle_test）と
+    レスポンスコード（500）を設定
+    """
+    return "error_handle_test", 500
+
+
+def error_badrequest(ex):
+    
+    """ハンドラが受け取った例外を return すると
+    例外（ex）に設定された内容がレスポンスになる
+    """
+    return ex
