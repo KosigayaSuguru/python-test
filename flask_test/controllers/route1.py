cbf260322b9269f3325058ffe9544d1edf14f330
self2 <self2>
2018-05-05 22:46:50 +0900

    flask-testにログ出力追加

--- flask_test/controllers/route1.py
+++ flask_test/controllers/route1.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask_test.models.TestJsonSerialize import TestJsonSerialize
 import pyckson
 from flask import json
+from flask import current_app
 
 app_route1 = Blueprint("route1", __name__, url_prefix="/route1")
 
@@ -15,7 +16,10 @@ def test():
 def test_json_serialize():
     """オブジェクトのJson変換（jackson的な）のテスト
     """
+
     # dictionary型に変換される
     a = TestJsonSerialize("p1", "p2")
+    # ログ出し
+    current_app.logger.info(vars(a))
     # dictionaryをjson文字列に変換する
     return json.jsonify(pyckson.serialize(a))
